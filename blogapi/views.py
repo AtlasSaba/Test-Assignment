3d21eeef3917ae7b7e4b84fbece00810a910dd17
AtlasSaba <atlassaba8@gmail.com>
2023-09-15 00:37:02 +0500

    add authentication for apis

--- blogapi/views.py
+++ blogapi/views.py
@@ -3,47 +3,60 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from .serializer import AllPostsSerializer
 from rest_framework.permissions import IsAuthenticated
+from .authentication import (
+    IsAuthenticatedExceptGet,
+    IsPostAuthor
+)
+from rest_framework import status
+
 
 class ListAllPosts(APIView):
+    permission_classes = [IsAuthenticatedExceptGet]
 
-	def get(self,request):
-		posts =  BlogPost.objects.all()
-		serializer = AllPostsSerializer(posts, many=True)
-		return Response(serializer.data)
+    def get(self, request):
+        posts = BlogPost.objects.all()
+        serializer = AllPostsSerializer(posts, many=True)
+        return Response(serializer.data)
 
-	# @permission_class([IsAuthenticated])
-	def post(self,request):
-		serializer = AllPostsSerializer(data=request.data)
-		if serializer.is_valid():
-			serializer.validated_data['author'] = request.user
-			serializer.save()
-			return Response(serializer.data)
-		return Response(serializer.errors)
+    def post(self, request):
+        serializer = AllPostsSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.validated_data['author'] = request.user
+            serializer.save()
+            return Response(serializer.data)
+
+        return Response(serializer.errors)
 
 
 class BlogPostView(APIView):
+    permission_classes = [IsAuthenticatedExceptGet, IsPostAuthor]
 
-	authetication_classes = []
-	permission_classes = []
-
-	def get(self,request, pk):
-		posts =  BlogPost.objects.get(pk=pk)
-		serializer = AllPostsSerializer(posts)
-		return Response(serializer.data)
-
-	def put(self,request, pk):
-		blog_post = BlogPost.objects.get(pk=pk)
-		serializer = AllPostsSerializer(posts, data=request.data)
-		if serializer.is_valid():
-			serializer.save()
-			return Response(serializer.data)
-
-		return Response(serializer.errors)
+    def get(self, request, pk):
+        try:
+            posts = BlogPost.objects.get(pk=pk)
+        except BlogPost.DoesNotExist:
+            return Response({"message": "Blog post not found"}, status=status.HTTP_404_NOT_FOUND)
+        serializer = AllPostsSerializer(posts)
+        return Response(serializer.data)
 
 
-	def delete(self,request, pk):
-		blog_post = BlogPost.objects.get(pk=pk)
-		blog_post.delete()
-		return Response({"message":"successfully deleted blog post"})
+    def put(self, request, pk):
+        try:
+            blog_post = BlogPost.objects.get(pk=pk)
+        except BlogPost.DoesNotExist:
+            return Response({"message": "Blog post not found"}, status=status.HTTP_404_NOT_FOUND)
+        serializer = AllPostsSerializer(blog_post, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
+        
+    def delete(self, request, pk):
+            try:
+                blog_post = BlogPost.objects.get(pk=pk)
+            except BlogPost.DoesNotExist:
+                return Response({"message": "Blog post not found"}, status=status.HTTP_404_NOT_FOUND)
+            blog_post.delete()
+            return Response({"message": "successfully deleted blog post"})
 
 
